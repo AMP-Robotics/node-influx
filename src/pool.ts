b45b88b713e098bc459a1e4a5e4bd335b61cf761
Brian <brian@amprobotics.com>
2023-10-24 13:18:18 -0600

    try accessing stringify like this

--- src/pool.ts
+++ src/pool.ts
@@ -5,7 +5,7 @@ import { Host } from "./host";
 import * as http from "http";
 import * as https from "https";
 // import * as querystring from "querystring";
-import { stringify } from "query-string/base";
+import * as queryString from "query-string";
 import * as urlModule from "url";
 
 /**
@@ -372,7 +372,8 @@ export class Pool {
     let path = host.url.pathname === "/" ? "" : host.url.pathname;
     path += options.path;
     if (options.query) {
-      path += "?" + stringify(options.query);
+      path += "?" + queryString.default.stringify(options.query)
+      // path += "?" + queryString.stringify(options.query);
     }
 
     const req = request(
